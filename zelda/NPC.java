5990e13e4669eadfc272134e8b4049b33b8a31e1
pugnaguido <144164356+pugnaguido@users.noreply.github.com>
2024-03-24 23:45:26 -0700

    Add files via upload

---
+++ zelda/NPC.java
@@ -0,0 +1,10 @@
+package zelda;
+
+
+//this is a child class of the person class
+public class NPC extends person {
+    //create out constructor to set the properties of the parent class
+    public NPC(String location, String name) {
+        super(location, name);
+    }
+}
